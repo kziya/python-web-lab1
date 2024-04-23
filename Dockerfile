a3f1fffb2c3fb44f8368da07e241bb4e14647578
Ziya Karimli <ziyakerimli04@gmail.com>
2024-04-23 17:13:44 +0300

    refactoring: Make folder structure

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,4 @@ RUN python -m pip install -r requirements.txt
 
 COPY . /app
 EXPOSE 3000
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0","--port", "3000"]
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from fastapi import FastAPI
+from app import app
 
-app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=3000)
