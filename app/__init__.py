ed2997a304b7e39f313cb3da4ed04658916d8215
Ziya Karimli <ziyakerimli04@gmail.com>
2024-04-23 23:28:07 +0300

    add: User model and sqlalchemy to requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from .user import user_router
+from .db import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
-
 
 app.include_router(user_router.router)
---
+++ app/user/user_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+
+from app.db import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(255), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    role = Column(String(255), nullable=False)
